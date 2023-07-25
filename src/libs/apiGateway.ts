4c45a8d1a14eeb37b69c02d1699058fe6c8be163
Valentin Mendoza <jmendozat13@gmail.com>
2023-07-24 19:34:23 -0500

    Added cors headers

--- src/libs/apiGateway.ts
+++ src/libs/apiGateway.ts
@@ -20,6 +20,7 @@ export const formatJSONErrorResponse = (response: Record<string, unknown>) => {
     statusCode: 500,
     headers: {
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify(response)
